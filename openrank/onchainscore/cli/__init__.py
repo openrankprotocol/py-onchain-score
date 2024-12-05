64735602dd4d6f218838b7bb78f1d8032c27c75d
Eugene Kim <ek@karma3labs.com>
2024-12-05 11:36:29 -0800

    feat: use Typer for CLI skeleton

--- openrank/onchainscore/cli/__init__.py
+++ openrank/onchainscore/cli/__init__.py
@@ -1,8 +1,34 @@
+import asyncio
+from functools import wraps
+from typing import Optional, Annotated
 
-def main():
-    import asyncio, sys
-    sys.exit(asyncio.run(_main()))
+import typer
 
 
-async def _main():
-    print("Hello, world!")
+def sync(f):
+    """Decorate an async function to turn it into a synchronous one.
+
+    Done by running the function in the default asyncio loop.
+
+    Use this, e.g. to decorate an asynchronous function to use it as
+    a Click/Typer command function.
+    """
+
+    @wraps(f)
+    def wrapper(*poargs, **kwargs):
+        return asyncio.run(f(*poargs, **kwargs))
+
+    return wrapper
+
+
+app = typer.Typer()
+
+
+@app.command()
+def version():
+    from .. import __version__
+    print(f"openrank-onchain-score {__version__}")
+
+
+def entrypoint():
+    return app()
